04ed36d2544cde0489536e92fcbd5bf3b316c736
Bella Amandine <bellamandy45@gmail.com>
2021-01-13 13:59:54 +0200

    Add profile model

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='photos/', defautl='default.png')
-    
+    bio = models.TextField(default="My Bio", blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=50, blank=True)
 
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self):
+        self.delete()
+        
