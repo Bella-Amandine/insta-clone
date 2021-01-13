ddbccf1b658856dd2189d94be7fac0182c975b2d
Bella Amandine <bellamandy45@gmail.com>
2021-01-13 14:01:43 +0200

    Migrate profile model

--- instaapp/models.py
+++ instaapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to='photos/', defautl='default.png')
+    profile_photo = models.ImageField(upload_to='photos/', default='default.png')
     bio = models.TextField(default="My Bio", blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=50, blank=True)
@@ -16,4 +16,3 @@ class Profile(models.Model):
     
     def delete_profile(self):
         self.delete()
-        
