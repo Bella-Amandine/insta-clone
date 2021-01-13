034abace508c11580b5613e1dcdcbfd0a52130bd
Bella Amandine <bellamandy45@gmail.com>
2021-01-13 14:40:35 +0200

    Add post model

--- instaapp/models.py
+++ instaapp/models.py
@@ -16,3 +16,38 @@ class Profile(models.Model):
     
     def delete_profile(self):
         self.delete()
+
+    @classmethod
+    def get_all_profiles(cls):
+        return cls.objects.all()
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    image_name = models.CharField(max_length=30, blank=True)
+    caption = models.CharField(max_length=200, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+
+    def save_post(self):
+        self.save()
+
+    @classmethod
+    def delete_post(cls, image_id):
+        cls.objects.filter(id = image_id).delete()
+    
+    @classmethod
+    def update_caption(cls,image_id, entered_caption):
+        cls.objects.filter(id = image_id).update(caption = entered_caption)
+
+    @classmethod
+    def get_single_post(cls, image_id):
+        found_image = cls.objects.get(pk = image_id)
+        return found_image
+
+    @classmethod
+    def get_all_posts(cls):
+        return cls.objects.all()
+
+    def get_number_of_likes(self):
+        return self.likes.count()
+    
