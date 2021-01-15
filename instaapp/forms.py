4709d28bf6a8d788d2196358bb0f1191405696b8
Bella Amandine <bellamandy45@gmail.com>
2021-01-15 18:04:21 +0200

    Add post form

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=40)
@@ -8,3 +9,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('image', 'caption')
