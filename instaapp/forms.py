5d17aeb8224c8390219baf914ae23523e4d1ab5a
Bella Amandine <bellamandy45@gmail.com>
2021-01-25 12:42:27 +0200

    Add updateProfile form

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Profile
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=40)
@@ -14,3 +14,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('image', 'caption')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
