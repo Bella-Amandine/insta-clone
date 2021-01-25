feefc5ca8b8c3352923576bba5a343a280ccd1dc
Bella Amandine <bellamandy45@gmail.com>
2021-01-25 14:10:37 +0200

    Define profile view function

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from .forms import SignupForm, PostForm
+from .forms import SignupForm, PostForm, ProfileForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, Comment, Follow
@@ -41,5 +41,25 @@ def index(request):
     else:
         form = PostForm()
     return render(request, 'instaapp/index.html', {'posts': posts, 'form': form, 'users': users})
+
+@login_required(login_url='login')
+def profile(request):
+    my_profile = Profile.objects.filter(user = request.user).first()
+    my_posts = Post.objects.filter(user = my_profile).all()
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            name = form.cleaned_data.get('name')
+            bio = form.cleaned_data.get('bio')
+            profile_pic = form.cleaned_data.get('profile_photo')
+
+            Profile.update_profile(my_profile.id, name, bio, profile_pic)
+
+            return redirect('profile')
+    else:
+        form = ProfileForm()
+        return render(request, 'instaapp/profile.html', {'my_profile': my_profile, 'my_posts': my_posts, 'form': form})
 
     
